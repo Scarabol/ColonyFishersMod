31db0113984cb90dd88683fcc11cedb94fd3b30c
Scarabol <scarabol@gmail.com>
2017-09-03 02:27:25 +0200

    increase version to 1.3

--- src/Fishers.cs
+++ src/Fishers.cs
@@ -42,7 +42,7 @@ namespace ScarabolMods
     [ModLoader.ModCallback (ModLoader.EModCallbackType.AfterStartup, "scarabol.fishers.registercallbacks")]
     public static void AfterStartup ()
     {
-      Pipliz.Log.Write ("Loaded Fishers Mod 1.2 by Scarabol");
+      Pipliz.Log.Write ("Loaded Fishers Mod 1.3 by Scarabol");
     }
 
     [ModLoader.ModCallback (ModLoader.EModCallbackType.AfterDefiningNPCTypes, "scarabol.fishers.registerjobs")]
